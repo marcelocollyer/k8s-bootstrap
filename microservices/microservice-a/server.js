56cfc9fc8019b9ee8363474e07f39bc264b48cf5
Marcelo Collyer <marcelocollyer@gmail.com>
2024-05-05 05:04:20 -0400

    adding tree microservices written in python, nodejs and golang

---
+++ microservices/microservice-a/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = process.env.PORT || 80;
+
+const microserviceBUrl = 'http://microservice-b:80';
+const microserviceId = 'Microservice-a v2'
+
+app.get('/', async (req, res) => {
+    try {
+        print(`${microserviceId}. calling Microservice B`);
+        const response = await axios.get(microserviceBUrl);
+        res.send(`${microserviceId}. Response from Microservice B: ${response.data}`);
+    } catch (error) {
+        res.status(500).send(`${microserviceId}. Failed to fetch data from Microservice B`);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`${microserviceId} listening on port ${port}`);
+});
